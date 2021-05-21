a2cc16c8496f3edfce4b031aa200e5fcb4c93dc1
Luna Sare <sanchez.reyes.luna@gmail.com>
2021-05-21 18:33:58 -0500

    reading the table in

--- R/hello.R
+++ R/hello.R
@@ -19,6 +19,17 @@ head(original_labels)
 
 # get labels suitable for OTU mapping
 
+# First, we need to read the csv file intro r
 
+table <- read.csv(file = "data-raw/mgharvey-tyranni-f73aa7f/Species_name_map_uids.csv")
+
+head(table)
+ls(table)
+class(table)
+
+# use dollar sign to extract the column containing the species names
+
+adjusted_labels <- table$matrix.species
+head(adjusted_labels)
 
 
